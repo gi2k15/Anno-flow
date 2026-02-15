c5e12c062ac544ce3db918bcd4364c1e6dfec818
gi2k15 <gendouikari2015@gmail.com>
2026-02-14 23:57:45 -0300

    Vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<style>
+body {
+  background-color: rgb(19, 19, 19);
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: white;
+}
+</style>
+
+<style scoped>
+.main-container {
+  width: 80%;
+}
+</style>
+
+<template>
+  <div class="main-containter">
+      <h1>Anno 117 Flow Calculator</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+
+
