bd462b29feaffbe405d18eea9a93cb6a157ed688
gi2k15 <gendouikari2015@gmail.com>
2026-02-16 02:32:30 -0300

    Added the component

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,46 @@ body {
 .main-containter {
   margin: 0 auto;
   width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>
 
 <template>
   <div class="main-containter">
-      <h1>Anno 117 Flow Calculator</h1>
-      <p>This app calculates how many of each build you need to construct based on the time
-        each one takes to make a product, so the chain is fully optimized.
-      </p>
+    <h1>Anno 117 Flow Calculator</h1>
+    <p>This app calculates how many of each build you need to construct based on the time
+      each one takes to make a product, so the chain is fully optimized.
+    </p>
+    <input type="button" value="Add building" @click="addBuilding" style="height: 2rem;">
+    <Building 
+      v-for="(building, index) in listBuildings" 
+      :key="index" 
+      v-model:industry-name="building.name"
+      v-model:time="building.time"
+      @remove="removeBuilding(index)">
+    </Building>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import Building from './Building.vue'
 
+function addBuilding() {
+  listBuildings.value.push({ ...buildingObject })
+}
 
+function removeBuilding(index) {
+  listBuildings.value.splice(index, 1)
+}
 
+const buildingObject = {
+  name: '',
+  time: null,
+}
+const listBuildings = ref([])
+
+addBuilding()
+</script>
--- src/Building.vue
+++ src/Building.vue
@@ -1,9 +1,55 @@
 <style scoped>
+.container {
+    display: flex;
+    justify-content: space-around;
+    gap: 10px;
+}
+
+.industry-container {
+    display: flex;
+    flex-direction: column;
+}
+
+.radio-container {
+    display: inline-flex;
+    gap: 10px;
+}
 </style>
 
 <template>
-    <div class="container"></div>
+    <div class="container">
+        <div class="industry-container">
+            <label for="industry-name">Industry</label>
+            <input type="text" id="industry-name" placeholder="Farm, Mine..." v-model="industryName">
+        </div>
+        <div class="industry-container">
+            <label>Time (seconds)</label>
+            <div class="radio-container">
+                <label>
+                    <input type="radio" name="time" class="time-radio" id="30" value="30" v-model="time">
+                    30
+                </label>
+                <label>
+                    <input type="radio" name="time" class="time-radio" id="60" value="60" v-model="time">
+                    60
+                </label>
+                <label>
+                    <input type="radio" name="time" class="time-radio" id="90" value="90" v-model="time">
+                    90
+                </label>
+                <label>
+                    <input type="radio" name="time" class="time-radio" id="120" value="120" v-model="time">
+                    120
+                </label>
+                <input type="number" id="time-custom" placeholder="Custom time" v-model="time">
+            </div>
+        </div>
+        <input type="button" value="Remove" @click="emit('remove')">
+    </div>
 </template>
 
 <script setup>
+const industryName = defineModel('industryName')
+const time = defineModel('time')
+const emit = defineEmits(['remove'])
 </script>
